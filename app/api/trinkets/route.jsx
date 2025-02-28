7722d5227342ce45dce630492ac6f7f95c084842
ayabarrera <alyannabarrera@gmail.com>
2025-02-27 21:06:46 -0700

    added route.jsx which houses all the fetching of data from our API.
    I also added error messages so that the users are provided with appropriate information when something goes wrong.

---
+++ app/api/trinkets/route.jsx
@@ -0,0 +1,31 @@
+export async function GET() {
+    const accessToken = process.env.DIRECTUS_ACCESS_TOKEN;
+    const directusUrl = "http://localhost:8055/items/AnekAnek";
+  
+    if (!accessToken) {
+      return new Response("Unauthorized", { status: 401 }); 
+    }
+  
+    try {
+      const res = await fetch(directusUrl, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+  
+      if (!res.ok) {
+        return new Response("Failed to retrieve data", { status: 502 }); 
+      }
+  
+      const data = await res.json();
+      return new Response(JSON.stringify(data), {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      return new Response("Server error", { status: 500 });
+    }
+  }
