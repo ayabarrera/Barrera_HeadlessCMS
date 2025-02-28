d71c4f78c230a1c22965e4c724438b54b5089066
ayabarrera <alyannabarrera@gmail.com>
2025-02-27 19:36:32 -0700

    added just the baseline for each item that I will try to render.

--- app/page.jsx
+++ app/page.jsx
@@ -1,1 +1,16 @@
 "use client";
+import './homepage.module.css';
+
+export default async function HomePage() {
+
+  return (
+    <div>
+      <h1>Aya's Trinket Collection</h1>
+            <p>Brand:</p>
+            <p>Kind:</p>
+            <p>Model:</p>
+            <p>image here</p>
+          </div>
+  );
+
+}
