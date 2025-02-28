960ecd2061d9f27b4ba983173956b77c6489d131
ayabarrera <alyannabarrera@gmail.com>
2025-02-27 20:13:58 -0700

    added data fetching for API and calling to render the info

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,42 @@
-"use client";
-import './homepage.module.css';
+import styles from "./homepage.module.css";
 
-export default async function HomePage() {
+async function getData() {
+  const res = await fetch("http://localhost:8055/items/AnekAnek");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch the data");
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+  const trinkets = data.data;
+  const directusUrl = "http://localhost:8055/assets/";
 
   return (
     <div>
-      <h1>Aya's Trinket Collection</h1>
-            <p>Brand:</p>
-            <p>Kind:</p>
-            <p>Model:</p>
-            <p>image here</p>
-          </div>
-  );
+      <h1 className={styles.h1Header}>Aya's Trinket Collection</h1>
 
+      <div className={styles.gridContainer}>
+        {trinkets.map((trinket) => (
+          <div key={trinket.id} className={styles.itemContainer}>
+            <h2>"{trinket.name}"</h2>
+            {trinket.image && (
+              <img
+                src={`${directusUrl}${trinket.image}`}
+                alt={trinket.name}
+                width={200}
+                height={200}
+              />
+            )}
+            {!trinket.image && <p>No Image Available</p>}
+            <p>Brand: {trinket.brand}</p>
+            <p>Model: {trinket.model}</p>
+            <p>Kind: {trinket.kind}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
