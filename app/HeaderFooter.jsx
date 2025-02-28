d4c3bbf5b8dc4ecd6a46afec2edb407b42e7958f
ayabarrera <alyannabarrera@gmail.com>
2025-02-27 21:51:41 -0700

    chaged how we call our about page in link.

--- app/HeaderFooter.jsx
+++ app/HeaderFooter.jsx
@@ -1,13 +1,14 @@
-import Link from 'next/link';
-import styles from './HeaderFooter.module.css'; 
+
+import Link from 'next/link'; 
+import styles from './HeaderFooter.module.css';
 
 export default function HeaderFooter({ children }) {
   return (
     <div>
-      <header className={styles.header}> 
-        <nav className={styles.nav}> 
-          <Link href="/" className={styles.link}>Home</Link>
-          <Link href="/about">About Me</Link>
+      <header className={styles.header}>
+        <nav className={styles.nav}>
+          <Link href="/" className={styles.link}>Home</Link> 
+          <Link href="/about" className={styles.link}>About Me</Link> 
         </nav>
       </header>
       <main>{children}</main>
