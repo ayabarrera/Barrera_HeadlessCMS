65d5a65f3790cd1616526e2aee1e02af337e4ed9
ayabarrera <alyannabarrera@gmail.com>
2025-02-27 21:08:55 -0700

    added better css properties.
    removed the background colour - pink to gray.
    
    Added back a border and shadow to give more depth.
    
    Also added hover effect to make it more interactive.

--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,31 @@
+"use client";
+
 import styles from "./homepage.module.css";
+import { useState, useEffect } from "react";
 
-async function getData() {
-  const res = await fetch("http://localhost:8055/items/AnekAnek");
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch the data");
-  }
-  return res.json();
-}
-
-export default async function Home() {
-  const data = await getData();
-  const trinkets = data.data;
+export default function Home() {
+  const [trinkets, setTrinkets] = useState([]);
   const directusUrl = "http://localhost:8055/assets/";
+
+  useEffect(() => {
+    async function fetchTrinkets() {
+      try {
+        const res = await fetch("/api/trinkets");
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await res.json();
+        setTrinkets(data.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchTrinkets();
+  }, []);
 
   return (
     <div>
       <h1 className={styles.h1Header}>Aya's Trinket Collection</h1>
-
       <div className={styles.gridContainer}>
         {trinkets.map((trinket) => (
           <div key={trinket.id} className={styles.itemContainer}>
@@ -31,7 +39,7 @@ export default async function Home() {
               />
             )}
             {!trinket.image && <p>No Image Available</p>}
-            <p>Brand: {trinket.brand}</p>
+            <p>{trinket.brand}</p>
             <p>Model: {trinket.model}</p>
             <p>Kind: {trinket.kind}</p>
           </div>
